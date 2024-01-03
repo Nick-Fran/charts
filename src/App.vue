ecce460648d643126bfb63ba0b4a1d61cadd497e
Nicolas Queiroz <nicolas.queirozvf@gmail.com>
2024-01-02 22:10:26 -0400

    Adding Navbar

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,12 @@
 <script setup lang="ts">
 import HelloWorld from './components/HelloWorld.vue'
+import Navbar from './components/Navbar.vue'
 </script>
 
 <template>
+  <Navbar title="Vite + Vue + Chart - Sample"/>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    
   </div>
   <HelloWorld msg="Vite + Vue" />
 </template>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+
+defineProps<{ title: string }>()
+</script>
+
+<template>
+  <nav class="navbar">
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="../assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
+    <span class="nav-title">{{ title }}</span>
+  </nav>
+</template>
+
+<style scoped>
+.navbar {
+  display: flex;
+  flex: 1;
+  align-items: center;
+  padding: 12px 24px;
+  background-color: #464e47;
+  color: #f1fffa;
+}
+
+.nav-title {
+  margin-left: 24px;
+}
+</style>
