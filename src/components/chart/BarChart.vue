ad063ed8d95957708985d345b9b59bcc7834dc1d
Nicolas Queiroz <nicolas.queirozvf@gmail.com>
2024-01-07 17:02:21 -0400

    Adding button controllers to chart

--- src/components/chart/BarChart.vue
+++ src/components/chart/BarChart.vue
@@ -1,4 +1,9 @@
 <template>
+  <div>
+    <button @click="refreshData">Refresh</button>
+    <button @click="addColumn">Add Column</button>
+    <button @click="removeColumn">Remove Column</button>
+  </div>
   <Bar id="my-chart-id" :options="chartOptions" :data="chartData" />
 </template>
 
@@ -6,21 +11,58 @@
 import { defineComponent } from 'vue';
 import { Bar } from 'vue-chartjs'
 import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
-import { GenerateSeries, GenerateLinearArray } from '../../utils/math.ts'
+import { GenerateRandom, GenerateSeries, GenerateLinearArray } from '../../utils/math.ts'
 
 ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale);
 
+const INITIAL_SIZE = 10;
+
 export default defineComponent({
   components: { Bar },
+  methods: {
+    refreshData() {
+      this.datasets = [{ data: GenerateSeries(this.size) }];
+    },
+    addColumn() {
+      this.size++;
+      this.labels.push(this.labels.length);
+      this.datasets = this.datasets.map((dataset, index) => {
+        let newData = [...dataset.data];
+        newData.push(GenerateRandom())
+        return { data: newData, order: index + 1 }
+      });
+    },
+    removeColumn() {
+      if (this.size > 1) {
+        this.size--;
+        this.labels.pop();
+        this.datasets = this.datasets.map((dataset) => {
+          let newData = [...dataset.data];
+          newData.pop()
+          return {
+            ...dataset,
+            data: newData
+          }
+        });
+      }
+    }
+  },
   data() {
     return {
-      chartData: {
-        labels: GenerateLinearArray(),
-        datasets: [{ data: GenerateSeries() }]
-      },
+      size: INITIAL_SIZE,
+      labels: GenerateLinearArray(INITIAL_SIZE),
+      datasets: [{ data: GenerateSeries(INITIAL_SIZE) }],
       chartOptions: {
         responsive: true
       }
+    }
+  },
+  computed: {
+    chartData: function () {
+      return ({
+        labels: this.labels,
+        datasets: this.datasets
+      })
     }
   }
 })
